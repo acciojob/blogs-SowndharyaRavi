669205c8f6dd0824f0a75e03478b7d5410961bf9
Sowndharya <sowndharyaktr@gmail.com>
2023-02-18 17:38:28 +0530

    first commit

---
+++ src/main/java/com/driver/models/Image.java
@@ -0,0 +1,64 @@
+package com.driver.models;
+
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "image")
+public class Image{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String describtion;
+
+    private String dimensions;
+
+    @ManyToOne
+    @JoinColumn
+    private Blog blog;
+
+    public Image() {
+    }
+
+    public Image(int id, String describtion, String dimensions, Blog blog) {
+        this.id = id;
+        this.describtion = describtion;
+        this.dimensions = dimensions;
+        this.blog = blog;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescribtion() {
+        return describtion;
+    }
+
+    public void setDescribtion(String describtion) {
+        this.describtion = describtion;
+    }
+
+    public String getDimensions() {
+        return dimensions;
+    }
+
+    public void setDimensions(String dimensions) {
+        this.dimensions = dimensions;
+    }
+
+    public Blog getBlog() {
+        return blog;
+    }
+
+    public void setBlog(Blog blog) {
+        this.blog = blog;
+    }
+}
--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -5,6 +5,7 @@ import com.driver.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,16 +17,44 @@ public class ImageService {
     ImageRepository imageRepository2;
 
     public Image addImage(Integer blogId, String description, String dimensions){
-        //add an image to the blog
+       Image image=new Image();
+       image.setDescribtion(description);
+       image.setDimensions(dimensions);
 
+       Blog blog = blogRepository2.findById(blogId).get();
+
+       //image.setBlog(blog);
+       List<Image>images=blog.getImageList();
+       images.add(image);
+       blog.setImageList(images);
+
+       blogRepository2.save(blog);
+       imageRepository2.save(image);
+
+       return image;
     }
 
     public void deleteImage(Integer id){
-
+          imageRepository2.deleteById(id);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+         String screenArray[]=screenDimensions.split("X");
 
+         Image image=imageRepository2.findById(id).get();
+
+         String imageArray[]=image.getDimensions().split("X");
+
+         int screenLength=Integer.parseInt(screenArray[0]);
+         int screenBreadth=Integer.parseInt(screenArray[1]);
+
+         int imageLength=Integer.parseInt(imageArray[0]);
+         int imageBreadth=Integer.parseInt(imageArray[1]);
+
+         int len=screenLength/imageLength;
+         int bre=screenBreadth/imageBreadth;
+
+         return len*bre;
     }
 }
